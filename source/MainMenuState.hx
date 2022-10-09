54fffd1effaba293f6a32c1e710e4c55a888874b
Mini Raposa Gamer <115371844+MiniRaposaGAMER@users.noreply.github.com>
2022-10-09 14:55:54 -0300

    shopping center norte

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -230,19 +230,6 @@ class MainMenuState extends MusicBeatState
 			//menuItem.setGraphicSize(Std.int(menuItem.width * 0.58));
 			menuItem.updateHitbox();
 
-			
-			//Gf Main Menu
-
-			gfDance = new FlxSprite(FlxG.width * 0.4, FlxG.height * 0.07);
-			gfDance.frames = Paths.getSparrowAtlas('gfDanceTitle');
-			gfDance.animation.addByIndices('danceLeft', 'gfDance', [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24, true);
-			add(gfDance);
-
-			if(gfDance != null) {
-				danceLeft = !danceLeft;
-	
-				if (danceLeft)
-					gfDance.animation.play('danceLeft');
 			}
 
 //if you did everything correctly you should have a very nice looking main menu 
